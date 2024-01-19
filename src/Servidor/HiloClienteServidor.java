74411986dc866de57eef02aaa788e67f861b844b
silvia <silvita78449@gmail.com>
2024-01-19 21:32:04 +0100

    Esquema JCHAT

--- src/Servidor/HiloClienteServidor.java
+++ src/Servidor/HiloClienteServidor.java
@@ -23,11 +23,31 @@ public class HiloClienteServidor extends Thread {
             dis = new DataInputStream(cliente.getInputStream());
             nombre = dis.readUTF();
             System.out.println("Recibido cliente " + nombre + " con dirección " + cliente.getRemoteSocketAddress());
+            if (!addCliente()) {
+
+            }
         } catch (IOException ex) {
             //Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println("Cerrando cliente " + nombre);
         }
         System.out.println(this.nombre + " desconectado.");
+    }
+
+    private boolean addCliente() {
+        synchronized (ServidorChat.CONEXIONES_CLIENTES) {
+            if (ServidorChat.CONEXIONES_CLIENTES.contains(nombre)) {
+                //ENVIAR MENSAJE DE QUE NO SE PUDO ACEPTAR LA CONEXION
+                return false;
+            }
+            ServidorChat.CONEXIONES_CLIENTES.add(this);
+        }
+        
+        synchronized (ServidorChat.HISTORIAL_CLIENTES) {
+                Integer conexiones = ServidorChat.HISTORIAL_CLIENTES.get(nombre);
+                conexiones = conexiones == null ? 1 : ++conexiones;
+                ServidorChat.HISTORIAL_CLIENTES.put(nombre, conexiones);
+            }
+            return true;
     }
 
 }
